eab8519932f02e51dc557e4d7eb6558ac993f45c
SandeepMlr <sandeep.hebbar@cognizant.com>
2017-10-31 14:48:45 +0530

    work in progress

--- components/albums/src/main/java/org/superbiz/moviefun/albums/AlbumsRepository.java
+++ components/albums/src/main/java/org/superbiz/moviefun/albums/AlbumsRepository.java
@@ -24,7 +24,7 @@ import javax.persistence.criteria.CriteriaQuery;
 import java.util.List;
 
 @Repository
-public class AlbumsBean {
+public class AlbumsRepository {
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -32,6 +32,7 @@ public class AlbumsBean {
     @Transactional
     public void addAlbum(Album album) {
         entityManager.persist(album);
+        //entityManager.merge(album);
     }
 
     public Album find(long id) {
